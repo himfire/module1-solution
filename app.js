ed1118cb3e1a9cf2650164106b368ae09c5b2a9c
himfire <hdalang@gmail.com>
2020-11-27 05:38:18 +0300

    This is my app

---
+++ app.js
@@ -0,0 +1,27 @@
+(function(){
+'use strict';
+
+angular.module("LunchCheck",[])
+.controller("LunchCheckController",LunchCheckController);
+LunchCheckController.$inject = ["$scope"];
+function LunchCheckController($scope){
+
+	$scope.check = function(){
+		var texta = $scope.item.split(', ').join(',');
+		var textb = texta.split(', ').join(',');
+		var len = textb.split(",");
+		$scope.food = len;
+		if(len.length == 0){
+			$scope.message =  "Empty";
+			$scope.color = "green";
+		}else if(len.length < 4){
+			$scope.message = "Enjoy";
+			$scope.color = "green";
+		}else{
+			$scope.message = "This it too much!";
+			$scope.color = "red";
+		}
+		
+	}
+}
+})();
